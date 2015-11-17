7e013436ff270d120329f54e4a3a886e6cc0663d
lalikir <lalikir@gmail.com>
2015-11-17 13:55:28 +0100

    First

---
+++ app/app.js
@@ -0,0 +1,227 @@
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app).listen(3000);
+var io = require('socket.io').listen(server);
+var fs = require('fs');
+var request = require("request");
+var path = require('path');
+var parsedJSON = require('./city.json');		//városok és ID-k vannak benne
+var JSONstring;									//a kiválasztott városról lekérdezett adatok vannak benne
+var clients = [];
+var is_client = 0;
+var temperatures = [];
+var dashboards = {};
+var dash_id;
+var AppId = "fd4d21cfaa35d69abf9dfd00a761cb65";
+
+
+// Using the .html extension instead of
+// having to name the views as *.ejs
+app.engine('.html', require('ejs').__express);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+ 
+// Set the folder where the pages are kept
+app.set('views', __dirname + '/views');
+ 
+// This avoids having to provide the 
+// extension to res.render()
+app.set('view engine', 'html');
+
+
+var city_id; 
+var ujvaltozo;  //
+
+app.get('/', function (req, res) {
+	dash_id = Math.floor((Math.random() * 100000000) + 1);
+//	res.send({dashboardid: dash_id});
+	res.redirect('/dash/'+ dash_id);
+
+});
+
+
+app.get('/dash/:id', function (req, res) {
+	console.log("dash"+req.params.id);
+
+	app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+	if (dashboards[req.params.id])
+    {
+        var choice=dashboards[req.params.id];
+//		console.log(req.params.id);
+    }else{
+		dashboards[req.params.id]=new Array();
+		choice=dashboards[req.params.id];
+	}
+	//console.log(dashboards);
+
+res.render("socket",{
+	dashboardid:req.params.id, mydashboard: JSON.stringify(choice)
+	});
+ // res.sendfile(__dirname + '/views/socket.html');;
+});
+
+	function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+
+
+
+function WaitResponse() {
+	
+}
+
+
+io.sockets.on('connection', function (socket) {
+	
+	
+	clients.push(socket);
+	//---------------aktív kapcsolatok karbantartásához--------------
+	console.log('Connected user is:', clients.length);
+	io.sockets.emit('info', parsedJSON);  //------------ezzel tudom az összes socketnek ugyanazt kiküldeni------------
+	
+	
+	/****** ha csak az adott socketnek szeretnem:
+	socket.emit('info', { msg: Math.floor((Math.random() * 100) + 1 )});
+	*******/
+	
+	socket.on('getcity', function(data)			//egy ugyanilyennel nyomon lehet követni a a dashboardokat   ______ 
+	{
+		//socket.emit('torefresh', { msg: "reggeli"});
+		console.log(data);
+//		dashboards[data.dashboardid]=data;
+
+		
+		
+		
+
+		//-------------végignézi a tárolt városok listáját, és a megfelelő város id mezőjét kiválasztja-----------
+		for(var i=0; i<parsedJSON.length; i++){
+			if (parsedJSON[i].name == data)
+				{
+//					console.log(parsedJSON[i]._id);
+					city_id = parsedJSON[i]._id
+				}
+//				else 	console.log("nincs");		
+		}
+		
+		
+		
+		var url = "http://api.openweathermap.org/data/2.5/forecast/daily?id=" + city_id + "&appid=" + AppId;
+
+
+
+		request({
+			url: url,
+			json: true
+		}, function (error, response, body) {
+
+
+
+			if (!error && response.statusCode === 200) {
+			
+			
+			var t = new Date( body.list[0].dt*1000 );
+//			var formatted = t.format("dd.mm.yyyy hh:MM:ss");
+
+				
+		        // Print the json response
+			   JSONstring = JSON.stringify(body);
+			   //console.log(JSONstring);
+
+				socket.emit("buildchart", JSONstring);
+			}
+
+		})
+					
+
+									
+
+
+		
+	});
+	
+		socket.on('dash', function(data){
+
+			var obj = {};
+			obj.cityname = data.cityname;
+			obj.elemid = data.elemid;
+			obj.top = data.top;
+			obj.left = data.left;
+			obj.wrapper_height  = 	0;
+
+			dashboards[data.id].push(obj);
+			console.log(dashboards);
+			
+			//console.log(dashboards[data.id].length);
+
+			
+			
+			});
+
+	socket.on("positions", function(data){
+			
+			//console.log(data[0].id);
+			//console.log(data.length);
+			//console.log(dashboards[data.id].length);
+
+		
+
+		//console.log(pos.elemid); 
+		console.log("elemek:");
+		console.log(data);
+ 		for (i=0; i<dashboards[data[0].id].length; i++){
+			if(dashboards[data[0].id][i].elemid == data[i].elemid){
+				dashboards[data[0].id][i].top = data[i].top;
+				dashboards[data[0].id][i].left = data[i].left;
+				dashboards[data[0].id][i].wrapper_height = data[i].charts;
+				
+			}
+			
+		/*	if(dashboards[i][0] == data[0].id)
+			{
+				console.log("izeeeeeeeeeeeeeeee" + dashboards[i]);
+			}
+			*/
+			
+			
+				
+				//var pos = JSON.parse(data);
+				//console.log(pos[1].elemid);
+				
+					
+					
+			}
+		}
+	)
+
+	
+
+	
+	
+	  socket.on('disconnect', function () {
+	  	for (i=0; i<clients.length; i++) {
+		if(clients[i].id == socket.id) 
+			{
+			clients.splice(i,1);				//-----------------kapcsolatot bontott socket törlése a listából--------------------
+			}
+		}
+		io.emit('user disconnected');
+		console.log("disc")
+
+	  });
+	
+
+
+});
---
+++ app/public/js/line_chart.js
@@ -0,0 +1,41 @@
+
+  
+  
+    google.load('visualization', '1.1', {packages: ['line']});
+    google.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'Day');
+      data.addColumn('number', 'Guardians of the Galaxy');
+ //     data.addColumn('number', 'The Avengers');
+ //     data.addColumn('number', 'Transformers: Age of Extinction');
+
+      data.addRows([
+        [1,  37.8 ],
+        [2,  30.9 ],
+        [3,  25.4 ],
+        [4,  11.7 ],
+        [5,  11.9 ],
+        [6,   8.8 ],
+        [7,   7.6 ],
+        [8,  12.3 ],
+        [9,  16.9 ],
+        [10, 12.8 ]
+
+      ]);
+
+      var options = {
+        chart: {
+          title: 'Box Office Earnings in First Two Weeks of Opening',
+          subtitle: 'in millions of dollars (USD)'
+        },
+        width: 900,
+        height: 500
+      };
+
+      var chart = new google.charts.Line(document.getElementById('linechart_material'));
+
+      chart.draw(data, options);
+    }
